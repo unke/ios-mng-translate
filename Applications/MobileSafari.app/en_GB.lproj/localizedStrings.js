c3bf389af11a3cef71edbb846d5a3feba282431a
unke <unenbatzogsokh@yahoo.com>
2012-04-09 17:17:05 +0800

    first commit

---
+++ Applications/MobileSafari.app/en_GB.lproj/localizedStrings.js
@@ -0,0 +1,5 @@
+var localizedStrings = new Object;
+
+localizedStrings["Loading Next Page..."] = "Loading Next Page...";
+localizedStrings["Page %@"] = "Page %@";
+localizedStrings["Page %@ of %@"] = "Page %@ of %@";
